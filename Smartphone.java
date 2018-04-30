ddd2bd5b3311d04d18e48d0e23715bc09bb96ae5
siskom17 <38357581+siskom17@users.noreply.github.com>
2018-04-30 16:26:55 +0700

    Add files via upload

---
+++ Smartphone.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartphone;
+
+/**
+ *
+ * @author KELOMPOK 28:)
+ */
+public class Smartphone {
+
+    /** 
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+          
+            isi w = new isi();
+            w.olah();
+        // Program Penjualan Smartphone Xiaomi
+        // Sirajuddin F. 21120117130071
+        // Heni Nurul 21120117120023
+    
+    }
+}
